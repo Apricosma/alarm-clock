9274c21375c7a28eabd54cde8b606e7b1e589daa
Apricosma <113317992+Apricosma@users.noreply.github.com>
2022-11-18 13:45:22 -0600

    Change styling a little and add utility functions

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,12 +1,26 @@
 'use strict';
 
+// Utility Functions
+// Add event listener
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+// element selector function
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+// Main clock
 function localTime() {
+    // gets all the times needed
     let date = new Date();
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
     let session = 'AM';
 
+    // AM/PM checker
     if (hh == 0) {
         hh = 12;
     }
@@ -19,9 +33,14 @@ function localTime() {
     mm = (mm < 10) ? "0" + mm : mm;
     ss = (ss < 10) ? "0" + ss : ss; 
 
+    // output
     let time = hh + ":" + mm + ":" + ss + "" + session;
 
+    // select document timer
     document.querySelector('.current-time').innerText = time; 
+
+    // updates every 1 second
     let t = setTimeout(function(){ localTime() }, 1000);
 }
 localTime();
+
